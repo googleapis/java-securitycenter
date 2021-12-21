91c6e730cb23a3d64c9a5805ccb9a8c27958bd49
SitaLakshmi <sita1996@gmail.com>
2021-12-21 14:03:53 +0530

    added bulk mute and mute/unmute samples

---
+++ samples/snippets/src/main/java/com/google/cloud/examples/securitycenter/snippets/muteconfig/BulkMuteFindings.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.examples.securitycenter.snippets.muteconfig;
+
+// [START securitycenter_bulk_mute]
+
+import com.google.cloud.securitycenter.v1.BulkMuteFindingsRequest;
+import com.google.cloud.securitycenter.v1.BulkMuteFindingsResponse;
+import com.google.cloud.securitycenter.v1.SecurityCenterClient;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+public class BulkMuteFindings {
+
+  public static void main(String[] args)
+      throws IOException, ExecutionException, InterruptedException {
+    // parentPath: use any one of the following three options
+    //    organizations/{organization} or
+    //    folders/{folder} or
+    //    projects/{project}
+    // muteRule: Expression that identifies findings that should be updated.
+    // TODO: Replace the variables within {}
+    String parentPath = "{parent_path}";
+    String muteRule = "{filter-condition}";
+    bulkMute(parentPath, muteRule);
+  }
+
+  // Kicks off an LRO (Long-Running-Operation) to bulk mute findings for a parent based on a filter.
+  // The parent can be either an organization, folder or project. The findings
+  // matched by the filter will be muted after the LRO is done.
+  public static void bulkMute(String parentPath, String muteRule)
+      throws IOException, ExecutionException, InterruptedException {
+    try (SecurityCenterClient client = SecurityCenterClient.create()) {
+
+      BulkMuteFindingsRequest bulkMuteFindingsRequest = BulkMuteFindingsRequest.newBuilder()
+          .setParent(parentPath)
+          // To create mute rules, see:
+          // https://cloud.google.com/security-command-center/docs/how-to-mute-findings#create_mute_rules
+          .setFilter(muteRule).build();
+
+      BulkMuteFindingsResponse response = client.bulkMuteFindingsAsync(bulkMuteFindingsRequest)
+          .get();
+      System.out.println("Bulk mute findings completed successfully ! " + response);
+    }
+  }
+}
+// [END securitycenter_bulk_mute]
---
+++ samples/snippets/src/main/java/com/google/cloud/examples/securitycenter/snippets/muteconfig/SetMuteUnmuteFinding.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.examples.securitycenter.snippets.muteconfig;
+
+// [START securitycenter_set_mute_unmute]
+
+import com.google.cloud.securitycenter.v1.Finding;
+import com.google.cloud.securitycenter.v1.Finding.Mute;
+import com.google.cloud.securitycenter.v1.SecurityCenterClient;
+import com.google.cloud.securitycenter.v1.SetMuteRequest;
+import java.io.IOException;
+
+public class SetMuteUnmuteFinding {
+
+  public static void main(String[] args) throws IOException {
+    // findingPath: The relative resource name of the finding. See:
+    // https://cloud.google.com/apis/design/resource_names#relative_resource_name
+    // Example:
+    //  "organizations/{organization_id}/sources/{source_id}/finding/{finding_id}",
+    //  "folders/{folder_id}/sources/{source_id}/finding/{finding_id}",
+    //  "projects/{project_id}/sources/{source_id}/finding/{finding_id}".
+    // TODO: Replace the variables within {}
+    String findingPath = "{path-to-the-finding}";
+    setMute(findingPath);
+  }
+
+  // Mute/unmute an individual finding.
+  // If a finding is already muted, muting it again (or vice versa) is a no-op.
+  // Various mute states are: MUTE_UNSPECIFIED/ MUTE/ UNMUTE.
+  public static void setMute(String findingPath) throws IOException {
+    try (SecurityCenterClient client = SecurityCenterClient.create()) {
+
+      SetMuteRequest setMuteRequest = SetMuteRequest.newBuilder()
+          .setName(findingPath)
+          .setMute(Mute.MUTED).build();
+
+      Finding finding = client.setMute(setMuteRequest);
+      System.out.println("Mute value for the finding: " + finding.getMute());
+    }
+  }
+}
+// [END securitycenter_set_mute_unmute]
